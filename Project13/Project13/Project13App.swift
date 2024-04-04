ee9ae44086f46ac55a17ad9d00695dbebf92a008
viktorporch <kavaka08@mail.ru>
2024-04-05 00:42:03 +0300

    complete p13

---
+++ Project13/Project13/Project13App.swift
@@ -0,0 +1,17 @@
+//
+//  Project13App.swift
+//  Project13
+//
+//  Created by Victor on 05.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Project13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
